55f936d08662bb34911a2cb985af8d8604c7977a
Narayan Swamy Ghanta <iamnswamy@outlook.com>
2023-05-10 10:03:33 +0200

    added scripts to initialize

---
+++ workspace/docker-scripts/initialize.sh
@@ -0,0 +1,5 @@
+bundler install | grep kitchen
+docker build --tag=ubuntu1404_salt_minion -f ./ubuntu-14.04/Dockerfile .
+docker build --tag=ubuntu1804_salt_minion -f ./ubuntu-18.04/Dockerfile .
+docker build --tag=ubuntu2004_salt_minion -f ./ubuntu-20.04/Dockerfile .
+docker build --tag=ubuntu2204_salt_minion -f ./ubuntu-22.04/Dockerfile .
